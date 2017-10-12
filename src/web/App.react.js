d3d21a4f0cd3ac5e03c8d759c36d98aacb1b034c
IncredibLink <incrediblelink@gmail.com>
2017-10-12 19:26:43 -0400

    Ugly react-scroll enabled on landing page

--- src/web/App.react.js
+++ src/web/App.react.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { Element, Link, animateScroll } from 'react-scroll';
 
 export default class App extends React.Component {
-
   render() {
     return (
       <div>
+        <Link activeClass="active"
+              to="introduction"
+              spy={true}
+              offset={50}
+              smooth={true}
+              duration={500}>
+              What's HackCWRU?
+        </Link>
+        <Link activeClass="active"
+              to="registration"
+              spy={true}
+              offset={50}
+              smooth={true}
+              duration={500}>
+              Register!
+        </Link>
+        <Element name="introduction" style={{height: 1000, width: 100, 'backgroundColor': 'blue'}}></Element>
+        <Element name="registration" style={{height: 1000, width: 100, 'backgroundColor': 'green'}}></Element>
+        <a onClick={animateScroll.scrollToTop}>Go to top.</a>
       </div>
     )
   }
-
 }
